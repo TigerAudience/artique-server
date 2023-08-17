33fb88d9f895a43865d2d464aead411878617310
johan1103 <johan1103@naver.com>
2023-08-17 20:58:10 +0900

    [Feat] member id duplicate test

--- src/test/java/com/artique/api/member/MemberTest.java
+++ src/test/java/com/artique/api/member/MemberTest.java
@@ -1,6 +1,10 @@
 package com.artique.api.member;
 
+import com.artique.api.entity.Member;
+import com.artique.api.member.exception.LoginException;
+import com.artique.api.member.exception.LoginExceptionCode;
 import com.artique.api.member.response.MemberDuplicate;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,7 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,7 +30,7 @@ public class MemberTest {
   @DisplayName("중복 없는 아이디 테스트")
   void validateId(){
     //given
-    String memberId = new String("ohmyGosh");
+    String memberId = "sample_member_id";
     MemberDuplicate memberDuplicate = new MemberDuplicate(memberId);
     when(memberRepository.findById(memberId)).thenReturn(Optional.empty());
 
@@ -35,6 +39,24 @@ public class MemberTest {
 
     //then
     assertThat(findMember.getMemberId()).isEqualTo(memberDuplicate.getMemberId());
+  }
+
+  @Test
+  @DisplayName("증복 아이디 테스트")
+  void name(){
+    //given
+    String memberId = "sample_member_id";
+    Member initMember = new Member(memberId,"sample_nickname","sample_profileUrl",
+            "sample_introduce","sample_password");
+    when(memberRepository.findById(memberId)).thenReturn(Optional.of(initMember));
+
+    //when
+    LoginException exception = Assertions.assertThrows(LoginException.class,
+            ()->memberService.checkDuplicateMember(memberId));
+    //then
+    LoginExceptionCode code = LoginExceptionCode.DUPLICATE_LOGIN_ID;
+    Assertions.assertEquals(exception.getErrorCode(), code.toString());
+
   }
 
 }

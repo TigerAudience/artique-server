6ea6bb98c4516464d6aa2a1c958a7e6c34223fd2
johan1103 <johan1103@naver.com>
2023-08-27 16:22:36 +0900

    [Feat] edit swagger api

--- src/main/java/com/artique/api/member/request/OauthMemberReq.java
+++ src/main/java/com/artique/api/member/request/OauthMemberReq.java
@@ -12,11 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class OauthMemberReq {
   @NotNull(message = "간편 로그인 기업은 필수 입력값입니다.")
-  @Schema(description = "간편 로그인 할 third party 이름, 현재 지원하는 third party name : kakao, google (* 전부 소문자! *)")
+  @Schema(description = "간편 로그인 할 third party 이름, 현재 지원하는 third party name : kakao, google, apple (* 전부 소문자! *)")
   @Parameter(required = true)
   private String thirdPartyName;
   @NotNull(message = "token값은 필수 입력값입니다.")
-  @Schema(description = "google의 경우에는 idToken값, kakao의 경우에는 accessToken 값")
+  @Schema(description = "google의 경우에는 idToken값, kakao의 경우에는 accessToken 값, apple의 경우에는 identityToken값")
   private String token;
 
 }

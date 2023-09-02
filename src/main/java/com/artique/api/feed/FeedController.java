6dd6a091be9ad07642f5e37588c00159df7004f6
johan1103 <johan1103@naver.com>
2023-09-03 00:09:25 +0900

    [Feat] create repo

---
+++ src/main/java/com/artique/api/feed/FeedController.java
@@ -0,0 +1,8 @@
+package com.artique.api.feed;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FeedController {
+
+}
---
+++ src/main/java/com/artique/api/feed/FeedService.java
@@ -0,0 +1,11 @@
+package com.artique.api.feed;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeedService {
+
+  public void mainFeeds(){
+    
+  }
+}

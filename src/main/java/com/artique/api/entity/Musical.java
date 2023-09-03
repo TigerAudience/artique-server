e70313c845110b2b51dc41cdefd9169b169fab98
johan1103 <johan1103@naver.com>
2023-09-03 18:26:03 +0900

    [Fix] add getter

--- src/main/java/com/artique/api/entity/Musical.java
+++ src/main/java/com/artique/api/entity/Musical.java
@@ -1,8 +1,6 @@
 package com.artique.api.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 import java.time.LocalDate;

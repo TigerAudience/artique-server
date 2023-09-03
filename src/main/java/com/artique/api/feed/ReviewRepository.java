b6a8747b0b447d710bb838afe294ddb2ceef3c28
johan1103 <johan1103@naver.com>
2023-09-03 19:13:56 +0900

    [Feat] statistics feature

--- src/main/java/com/artique/api/feed/ReviewRepository.java
+++ src/main/java/com/artique/api/feed/ReviewRepository.java
@@ -1,6 +1,7 @@
 package com.artique.api.feed;
 
 import com.artique.api.entity.Review;
+import com.artique.api.feed.dao.FeedShorts;
 import com.artique.api.musical.dto.MusicalReview;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,8 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review,Long> {
   @Query(value = "select r from Review r join fetch r.musical mus join fetch r.member mem")
@@ -21,7 +24,7 @@ public interface ReviewRepository extends JpaRepository<Review,Long> {
           "r.viewDate,r.starRating,r.thumbsUp,r.shortReview,r.id,t.id)" +
           "from Review r join r.musical mus join r.member mem " +
           "left join r.thumbs t on t.member.id = :member_id order by r.thumbsUp")
-  Slice<FeedDto> findPageReviewsByMemberSlice(Pageable pageable, @Param("member_id") String memberId);
+  Slice<FeedShorts> findPageReviewsByMemberSlice(Pageable pageable, @Param("member_id") String memberId);
 
   @Query(value = "select new com.artique.api.musical.dto.MusicalReview" +
           "(mem.nickname,mem.profileUrl,mem.id,r.viewDate,r.starRating,r.thumbsUp,r.shortReview,r.id,t.id) " +
@@ -31,4 +34,7 @@ public interface ReviewRepository extends JpaRepository<Review,Long> {
           countQuery = "select count(r.id) from Review r join r.musical mus where mus.id = :musical_id")
   Page<MusicalReview> findPageMusicalReviewsByMusicalId(Pageable pageable, @Param("musical_id")String musicalId,
                                                         @Param("member_id")String memberId);
+
+  @Query(value = "select r from Review r where r.musical.id = :musical_id")
+  List<Review> findReviewsByMusicalId(@Param("musical_id")String musicalId);
 }

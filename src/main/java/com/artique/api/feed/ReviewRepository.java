1d7035a5f181b6974ceb044d84c4e414d4dda672
johan1103 <johan1103@naver.com>
2023-09-03 16:03:29 +0900

    [Feat] query with dto

--- src/main/java/com/artique/api/feed/ReviewRepository.java
+++ src/main/java/com/artique/api/feed/ReviewRepository.java
@@ -13,4 +13,12 @@ public interface ReviewRepository extends JpaRepository<Review,Long> {
   @Query(value = "select r from Review r join fetch r.musical mus join fetch r.member mem " +
           "left join r.thumbs t on t.member.id = :member_id")
   Slice<Review> findPageReviewBySliceWithUser(Pageable pageable, @Param("member_id") String memberId);
+
+  @Query(value = "select new com.artique.api.feed.FeedDto(mem.nickname,mem.profileUrl,mem.id," +
+          "mus.name,mus.posterUrl,mus.casting,mus.id," +
+          "r.viewDate,r.starRating,r.thumbsUp,r.shortReview,r.id,t.id)" +
+          "from Review r join r.musical mus join r.member mem " +
+          "left join r.thumbs t on t.member.id = :member_id order by r.thumbsUp")
+  Slice<FeedDto> findPageReviewsByMemberSlice(Pageable pageable, @Param("member_id") String memberId);
+
 }

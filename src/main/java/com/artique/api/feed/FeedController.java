38018a439d2a0ba4f934e99a3fc43bd1039690b2
johan1103 <johan1103@naver.com>
2023-09-03 16:04:25 +0900

    [Feat] feat controller, get parameter with primitive value

--- src/main/java/com/artique/api/feed/FeedController.java
+++ src/main/java/com/artique/api/feed/FeedController.java
@@ -1,11 +1,10 @@
 package com.artique.api.feed;
 
-import com.artique.api.entity.Review;
+import com.artique.api.resolver.LoginUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,7 +12,8 @@ public class FeedController {
   private final FeedService feedService;
 
   @GetMapping("/feed")
-  public FeedListDto feeds(){
-    return feedService.mainFeedsWithMember();
+  public FeedSliceDto feeds(@LoginUser String memberId, @RequestParam(value = "page")int page,
+                            @RequestParam(value = "size")int size){
+    return feedService.mainFeedsWithMember(memberId,page,size);
   }
 }

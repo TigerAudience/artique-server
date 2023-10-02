8a5046392d8567b59509cae208fa23be8427c3c2
johan1103 <johan1103@naver.com>
2023-10-02 16:41:05 +0900

    [Config] scouter

--- startUp.sh
+++ startUp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -javaagent:/Users/kimtaehyung/Documents/scouter/agent.java/scouter.agent.jar -Dscouter.config=/Users/kimtaehyung/Documents/scouter/agent.java/conf/scouter.conf --add-opens=java.base/java.lang=ALL-UNNAMED -Djdk.attach.allowAttachSelf=true -jar ./build/libs/api-0.0.1-SNAPSHOT.jar
+/Users/kimtaehyung/Library/Java/JavaVirtualMachines/corretto-17.0.4/Contents/Home/bin/java -javaagent:/Users/kimtaehyung/Documents/scouter/agent.java/scouter.agent.jar -Dscouter.config=/Users/kimtaehyung/Documents/scouter/agent.java/conf/scouter.conf --add-opens=java.base/java.lang=ALL-UNNAMED -Djdk.attach.allowAttachSelf=true -jar ./build/libs/api-0.0.1-SNAPSHOT.jar

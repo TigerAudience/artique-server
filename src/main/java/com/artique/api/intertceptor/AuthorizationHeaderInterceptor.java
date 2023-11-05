ad072814a8680e4819bedae921265c3d25947fdd
johan1103 <johan1103@naver.com>
2023-11-05 13:25:02 +0900

    [Feat] authorization interceptor

---
+++ src/main/java/com/artique/api/intertceptor/AuthorizationHeaderInterceptor.java
@@ -0,0 +1,30 @@
+package com.artique.api.intertceptor;
+
+import com.artique.api.member.exception.LoginException;
+import com.artique.api.session.CustomSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class AuthorizationHeaderInterceptor implements HandlerInterceptor {
+  private final CustomSession session;
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    if(request.getMethod().equals("OPTIONS"))
+      return HandlerInterceptor.super.preHandle(request, response, handler);
+    String sessionId = getSessionIdFromAuthorizationHeader(request);
+    if(sessionId == null || !session.validateSessionId(sessionId)){
+      throw new LoginException("authorization failure", HttpStatus.UNAUTHORIZED.toString());
+    }
+    return HandlerInterceptor.super.preHandle(request,response,handler);
+  }
+  private String getSessionIdFromAuthorizationHeader(HttpServletRequest request){
+    return request.getHeader("authorization");
+  }
+}

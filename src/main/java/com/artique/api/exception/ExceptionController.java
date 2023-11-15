03c0d050c4a37c2a2e4206210b3fabf58ef05339
johan1103 <johan1103@naver.com>
2023-11-16 02:24:22 +0900

    [Feat] add ExceptionClass & override HttpMessageNotReadableException for RequestBody Exception

--- src/main/java/com/artique/api/exception/ExceptionController.java
+++ src/main/java/com/artique/api/exception/ExceptionController.java
@@ -9,10 +9,12 @@ import com.artique.api.musical.MusicalException;
 import com.artique.api.thumbs.ThumbsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -58,6 +60,12 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
     return new ResponseEntity<>(ErrorResponse.builder().code(e.getInterceptorName()).message(e.getMessage()).build(),
             HttpStatus.BAD_REQUEST);
   }
+  @ExceptionHandler(InvalidDataAccessApiUsageException.class)
+  public ResponseEntity<ErrorResponse> invalidRequestBody(InvalidDataAccessApiUsageException e){
+    String message = "invalid request body : "+e.getMessage();
+    return new ResponseEntity<>(ErrorResponse.builder().code("REQUEST_BODY_MISMATCHING").message(message).build(),
+            HttpStatus.BAD_REQUEST);
+  }
 
   public ResponseEntity<Object> missingQueryParam(MissingServletRequestParameterException e){
     String message = "missing query parameter ["+e.getParameterName()+"]";
@@ -68,8 +76,19 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
     String paramName = e.getName();
     String value = String.valueOf(e.getValue());
     String message="failed to convert, parameter name is ["+paramName+"]. given value : "+value;
-    return new ResponseEntity<>(ErrorResponse.builder().code("PARAM_MISMATCHING").message(message).build(),
+    return new ResponseEntity<>(ErrorResponse.builder().code("PARAM_MISMATCHING").message(message),
             HttpStatus.BAD_REQUEST);
+  }
+  public ResponseEntity<Object> cannotResolveRequestBody(HttpMessageNotReadableException e){
+    String message = "invalid request body : "+e.getMessage();
+    HttpPretty httpPretty;
+    try {
+      httpPretty=httpRequestRepository.getPrettyString();
+    }catch (IOException exception){
+      httpPretty=null;
+    }
+    return new ResponseEntity<>(ErrorResponse.builder().code("REQUEST_BODY_MISMATCHING").message(message)
+            .httpRequest(httpPretty).build(), HttpStatus.BAD_REQUEST);
   }
 
   @Override
@@ -83,5 +102,10 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
       return handleTypeMismatch((MethodArgumentTypeMismatchException) ex);
     }
     return super.handleTypeMismatch(ex, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    return cannotResolveRequestBody(ex);
   }
 }

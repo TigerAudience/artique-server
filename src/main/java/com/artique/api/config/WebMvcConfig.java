ad09fd9f86ad752dea097e239e5a9b81a253c811
johan1103 <johan1103@naver.com>
2023-11-20 01:32:19 +0900

    [Hotfix] blocked swagger page

--- src/main/java/com/artique/api/config/WebMvcConfig.java
+++ src/main/java/com/artique/api/config/WebMvcConfig.java
@@ -33,6 +33,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
   private final AuthorizationHeaderInterceptor authorizationHeaderInterceptor;
   private final ReportTypeConverter reportTypeConverter;
   private final InvalidUrlInterceptor invalidUrlInterceptor;
+  private  final String[] excludePatternsForStaticResource = {"/css/**", "/images/**", "/js/**","/favicon.ico",
+          "/webjars/**","/error/**", "/oauth-redirect/**", "/swagger-ui/**",
+          "/swagger-resources/**","/v3/api-docs/**","META-INF/**"};
 
   @Override
   public void addFormatters(FormatterRegistry registry) {
@@ -46,13 +49,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry){
     String[] excludePatterns = buildExcludePattern();
     registry.addInterceptor(httpRequestInfoInterceptor).order(1).excludePathPatterns();
-    registry.addInterceptor(invalidUrlInterceptor).order(2).excludePathPatterns();
+    registry.addInterceptor(invalidUrlInterceptor).order(2).excludePathPatterns(excludePatternsForStaticResource);
     registry.addInterceptor(authorizationHeaderInterceptor).order(3).excludePathPatterns(excludePatterns);
   }
   public String[] buildExcludePattern(){
-    String[] excludePatternsForStaticResource = {"/css/**", "/images/**", "/js/**","/favicon.ico","/webjars/**","/error/**",
-            "/oauth-redirect/**", "/swagger-ui/**","/swagger-resources/**","/v3/api-docs/**"};
-    String[] excludePatternsForUri = {"/member/**","/feed/**","/review/**","/musical/**","/search/**","META-INF/**",
+    String[] excludePatternsForUri = {"/member/**","/feed/**","/review/**","/musical/**","/search/**",
             "/config/**","/session/**"};
     List<String> patterns = new ArrayList<>();
     patterns.addAll(Arrays.asList(excludePatternsForStaticResource));

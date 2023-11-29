81f5d9a4b9bc13135a3eb69884ac90416c06624f
johan1103 <johan1103@naver.com>
2023-11-29 17:29:26 +0900

    [Hotfix] review detail field

--- src/main/java/com/artique/api/reviewDetail/dto/ReviewDetailDto.java
+++ src/main/java/com/artique/api/reviewDetail/dto/ReviewDetailDto.java
@@ -10,7 +10,9 @@ import java.time.LocalDate;
 @Getter
 public class ReviewDetailDto {
   private Long id;
+  private String memberId;
   private String memberNickname;
+  private String musicalPosterUrl;
   private String musicalTitle;
   private String casting;
   private String seat;
@@ -22,8 +24,10 @@ public class ReviewDetailDto {
   private Boolean longSpoiler;
 
   public static ReviewDetailDto of(Review r){
-    return new ReviewDetailDto(r.getId(),r.getMember().getNickname(),r.getMusical().getName(),r.getCasting(),
-            r.getSeat(),r.getStarRating(),r.getShortReview(),r.getLongReview(),r.getViewDate(),r.isShortSpoiler(),
-            r.isLongSpoiler());
+    return new ReviewDetailDto(r.getId(),
+            r.getMember().getId(),r.getMember().getNickname(),
+            r.getMusical().getName(),r.getMusical().getPosterUrl(),
+            r.getCasting(),r.getSeat(),r.getStarRating(),r.getShortReview(),r.getLongReview(),r.getViewDate(),
+            r.isShortSpoiler(), r.isLongSpoiler());
   }
 }

9f68902f2d48411a5e5095b677d9ceac6c7a9490
johan1103 <johan1103@naver.com>
2023-12-05 15:41:11 +0900

    [Fix] move redis config annotation to RedisConfig.java

--- src/main/java/com/artique/api/config/RedisConfig.java
+++ src/main/java/com/artique/api/config/RedisConfig.java
@@ -1,2 +1,15 @@
-package com.artique.api.config;public class RedisConfig {
+package com.artique.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
+
+@Configuration
+@EnableRedisIndexedHttpSession
+public class RedisConfig {
+  @Bean
+  public LettuceConnectionFactory connectionFactory() {
+    return new LettuceConnectionFactory();
+  }
 }

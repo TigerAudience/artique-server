a3e4a893648c8984004e10dffbe93f2292144620
johan1103 <johan1103@naver.com>
2024-01-06 17:18:20 +0900

    [Fix] feed sort by created at

--- src/main/java/com/artique/api/feed/FeedService.java
+++ src/main/java/com/artique/api/feed/FeedService.java
@@ -23,14 +23,23 @@ public class FeedService {
   private final ReviewRepository reviewRepository;
 
   public FeedSliceDto mainFeedsWithMember(String memberId,int page,int size){
-    PageRequest pageRequest = PageRequest.of(page,size, Sort.by(Sort.Direction.DESC,"thumbsUp"));
 
     //최근 일주일동안 작성된 리뷰중, 공감 수 많은 순서로 리뷰 가져오기
     //회의 결과, 리뷰가 지속적으로 쌓일 정도로 서비스가 조금 더 성장했을 때 사용할 전략으로 결정
     /*
+    PageRequest pageRequest = PageRequest.of(page,size, Sort.by(Sort.Direction.DESC,"thumbsUp"));
     Slice<FeedShortsDao> feeds = reviewRepository
             .findPageReviewsByMemberSlice(pageRequest, ZonedDateTime.now().minusDays(7),ZonedDateTime.now());
      */
+    //작성된 모든 리뷰 중, 공감 수 많은 순서로 리뷰 가져오기
+    //2024/01/04 회의 결과, 피드의 리뷰 회전율이 너무 낮아 해당 방식은 보류하기로 결정
+    /*
+    PageRequest pageRequest = PageRequest.of(page,size, Sort.by(Sort.Direction.DESC,"thumbsUp"));
+    Slice<FeedShortsDao> feeds = reviewRepository
+            .findPageReviewsByMemberSliceAllDate(pageRequest);
+     */
+    //작성된 모든 리뷰 중, 가장 최근에 작성된 순서로 리뷰 가져오기
+    PageRequest pageRequest = PageRequest.of(page,size, Sort.by(Sort.Direction.DESC,"createdAt"));
     Slice<FeedShortsDao> feeds = reviewRepository
             .findPageReviewsByMemberSliceAllDate(pageRequest);
 

085c228ae14a58d2591f9d5f5740f1550a6a5ed3
johan1103 <johan1103@naver.com>
2024-02-18 15:30:44 +0900

    [Feat] 공감 수, 사용자 공감 여부 판단하기 위한 Dto 생성(of static method 사용)

--- src/main/java/com/artique/api/reviewDetail/ReviewService.java
+++ src/main/java/com/artique/api/reviewDetail/ReviewService.java
@@ -1,18 +1,25 @@
 package com.artique.api.reviewDetail;
 
 import com.artique.api.entity.Review;
+import com.artique.api.feed.FeedService;
 import com.artique.api.feed.ReviewRepository;
 import com.artique.api.reviewDetail.dto.ReviewDetailDto;
+import com.artique.api.reviewDetail.dto.ReviewDetailThumbsInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
   private final ReviewRepository reviewRepository;
-  public ReviewDetailDto getDetail(Long reviewId){
+  private final FeedService feedService;
+  public ReviewDetailDto getDetail(Long reviewId, String memberId){
     Review review = reviewRepository.findReviewByIdJoinFetchMemberMusical(reviewId)
             .orElseThrow(()->new ReviewException("REVIEW-001","invalid review id"));
-    return ReviewDetailDto.of(review);
+
+    Optional<ReviewDetailThumbsInfo> thumbsInfo = reviewRepository.findThumbsByMemberId(memberId).stream().findFirst();
+    return ReviewDetailDto.of(review,thumbsInfo);
   }
 }

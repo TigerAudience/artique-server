216b4f8beee0b9edead5c2536c4b7f2ac1131985
KTH <johan1103@naver.com>
2024-03-18 16:40:04 +0900

    [Feat] dto에서 사용하기 위해 getter 추가

--- src/main/java/com/artique/api/entity/ArtiqueRecommendMusical.java
+++ src/main/java/com/artique/api/entity/ArtiqueRecommendMusical.java
@@ -3,9 +3,11 @@ package com.artique.api.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor
 public class ArtiqueRecommendMusical {
   @Id

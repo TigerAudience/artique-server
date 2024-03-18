14d93d8a26ff5591aae785a9247bf65b8e0b33fb
KTH <johan1103@naver.com>
2024-03-18 16:39:41 +0900

    [Feat] 아티크 추천 작품용 엔티티 레포지토리 구현

---
+++ src/main/java/com/artique/api/home/ArtiqueRecommendMusicalRepository.java
@@ -0,0 +1,7 @@
+package com.artique.api.home;
+
+import com.artique.api.entity.ArtiqueRecommendMusical;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtiqueRecommendMusicalRepository extends JpaRepository<ArtiqueRecommendMusical,Long> {
+}
